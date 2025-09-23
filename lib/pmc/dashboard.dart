5a02b85b88efcb1b12aca7dd8b3f50f6bf0d5491
Avinash Magar <avinashsitsolutions@gmail.com>
2025-09-23 11:14:16 +0530

    render issue solved for ios & android

--- lib/pmc/dashboard.dart
+++ lib/pmc/dashboard.dart
@@ -1,39 +1,28 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:tankerpmc/pmc/builderlist.dart';
 import 'package:tankerpmc/pmc/dailysupply.dart';
 import 'package:tankerpmc/pmc/orderlist.dart';
-
 import 'package:http/http.dart' as http;
 import 'package:tankerpmc/pmc/stplist.dart';
 import 'package:tankerpmc/pmc/tankerlist.dart';
 import 'package:tankerpmc/pmc/todayreceipt.dart';
 import 'package:tankerpmc/widgets/constants.dart';
-import 'package:tankerpmc/widgets/dimensions.dart';
 import 'package:url_launcher/url_launcher.dart';
-
 import '../widgets/drawerWidget.dart';
 
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-
 class Dashboard extends StatefulWidget {
-  const Dashboard({
-    super.key,
-    // required this.myList
-  });
-  // final List<dynamic> myList;
+  const Dashboard({super.key});
+
   @override
   State<Dashboard> createState() => _DashboardState();
 }
 
 class _DashboardState extends State<Dashboard> {
-  bool get wantKeepAlive => true;
   List<dynamic> count = [];
 
-  bool loading = true;
   List<String> imagePaths = [
     'assets/buildings.png',
     'assets/truckfast.png',
@@ -42,6 +31,7 @@ class _DashboardState extends State<Dashboard> {
     'assets/Receipt long.png',
     'assets/Opacity.png',
   ];
+
   List<String> text = [
     'Total No.of Registered Builders',
     'Total No.of Registered Tankers',
@@ -50,22 +40,15 @@ class _DashboardState extends State<Dashboard> {
     'Today Orders',
     'Today\'s Supply(in liters)',
   ];
-//  'Total No.of Registered Tankers',
-//     'Total Orders',
-//     'Daily Supply(in liters)',
-//     'Today Orders',
-//     'Total No.of Registered STP',
-//     'Total No.of Registered Builders',
+
   @override
   void initState() {
-    finalList();
     super.initState();
+    finalList();
   }
 
   getCounts() async {
-    var response = await http.get(
-      Uri.parse("${Config.baseUrl}/count_new"),
-    );
+    var response = await http.get(Uri.parse("${Config.baseUrl}/count_new"));
     final Map<String, dynamic> data = jsonDecode(response.body);
     return data;
   }
@@ -73,7 +56,6 @@ class _DashboardState extends State<Dashboard> {
   finalList() {
     getCounts().then((data) {
       final List<String> dataList = [];
-
       data.forEach((key, value) {
         dataList.add(value.toString());
       });
@@ -92,15 +74,11 @@ class _DashboardState extends State<Dashboard> {
           content: const Text('Do you want to exit the app?'),
           actions: <Widget>[
             ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
+              onPressed: () => Navigator.of(context).pop(false),
               child: const Text('No'),
             ),
             ElevatedButton(
-              onPressed: () {
-                SystemNavigator.pop();
-              },
+              onPressed: () => SystemNavigator.pop(),
               child: const Text('Yes'),
             ),
           ],
@@ -112,7 +90,9 @@ class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
-    // super.build(context);
+    final screenWidth = Get.width;
+    final screenHeight = Get.height;
+
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
@@ -123,16 +103,14 @@ class _DashboardState extends State<Dashboard> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               InkWell(
-                // ignore: deprecated_member_use
-                onTap: () => launch('https://pcmcindia.gov.in/index.php'),
+                onTap: () =>
+                    launchUrl(Uri.parse('https://pcmcindia.gov.in/index.php')),
                 child: const Image(
                   image: AssetImage('assets/pcmc_logo.png'),
                   height: 50,
                 ),
               ),
-              const SizedBox(
-                width: 5,
-              ),
+              const SizedBox(width: 5),
               const Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -155,11 +133,7 @@ class _DashboardState extends State<Dashboard> {
             Builder(
               builder: (BuildContext context) {
                 return IconButton(
-                  icon: const Icon(
-                    Icons.menu,
-                    size: 25,
-                    color: Colors.white,
-                  ),
+                  icon: const Icon(Icons.menu, size: 25, color: Colors.white),
                   onPressed: () {
                     Scaffold.of(context).openEndDrawer();
                   },
@@ -170,127 +144,109 @@ class _DashboardState extends State<Dashboard> {
         ),
         endDrawer: const DrawerWid(),
         body: Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10),
-          child: LayoutBuilder(builder: (context, constraints) {
-            return count.isNotEmpty
-                ? GridView.builder(
-                    itemCount: count.length,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisSpacing: 20,
-                      crossAxisCount: 2,
-                      childAspectRatio:
-                          constraints.maxWidth / (constraints.maxHeight * 0.7),
-                    ),
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          switch (index) {
-                            case 0:
-                              Navigator.push(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02),
+          child: count.isNotEmpty
+              ? GridView.builder(
+                  itemCount: count.length,
+                  // ✅ allow scrolling
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisSpacing: screenWidth * 0.04,
+                    crossAxisCount: screenWidth > 1200
+                        ? 4 // desktops/large screens
+                        : screenWidth > 800
+                            ? 3 // tablets/iPads
+                            : 2, // phones
+                    childAspectRatio: screenWidth / (screenHeight * 0.55),
+                  ),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        switch (index) {
+                          case 0:
+                            Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => const BuilderList()),
-                              );
-
-                              break;
-                            case 1:
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          const TankerList()));
-
-                              break;
-                            case 2:
-                              Navigator.push(
+                                    builder: (context) => const BuilderList()));
+                            break;
+                          case 1:
+                            Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => const StpList()),
-                              );
-
-                              break;
-                            case 3:
-                              Navigator.push(
+                                    builder: (context) => const TankerList()));
+                            break;
+                          case 2:
+                            Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => const OrderList()),
-                              );
-
-                              break;
-                            case 4:
-                              Navigator.push(
+                                    builder: (context) => const StpList()));
+                            break;
+                          case 3:
+                            Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => const TodayReceipt()),
-                              );
-                              break;
-                            case 5:
-                              Navigator.push(
+                                    builder: (context) => const OrderList()));
+                            break;
+                          case 4:
+                            Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                    builder: (context) => const DailySupply()),
-                              );
-                              break;
-                          }
-                        },
-                        child: SizedBox(
-                            child: Padding(
-                          padding: const EdgeInsets.only(top: 30),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.blue[50],
-                              borderRadius: BorderRadius.circular(20),
+                                    builder: (context) =>
+                                        const TodayReceipt()));
+                            break;
+                          case 5:
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const DailySupply()));
+                            break;
+                        }
+                      },
+                      child: Container(
+                        margin: EdgeInsets.only(top: screenHeight * 0.03),
+                        decoration: BoxDecoration(
+                          color: Colors.blue[50],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              radius: screenWidth * 0.07,
+                              child: Image.asset(
+                                imagePaths[index],
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                            child: Column(
-                              children: [
-                                const SizedBox(
-                                  height: 40,
-                                ),
-                                CircleAvatar(
-                                    radius: 20,
-                                    child: Image.asset(
-                                      imagePaths[index],
-                                      fit: BoxFit.cover,
-                                      scale: 1.2,
-                                    )),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  count[index],
-                                  style: const TextStyle(
-                                      fontSize: 25,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Text(
-                                  text[index].toString(),
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
+                            SizedBox(height: screenHeight * 0.015),
+                            Text(
+                              count[index],
+                              style: TextStyle(
+                                fontSize: screenWidth * 0.05,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                          ),
-                        )),
-                      );
-                    },
-                  )
-                : SizedBox(
-                    height: Dimensions.screenHeight / 2,
-                    child: const Center(
-                        // widthFactor: ,
-                        child: CircularProgressIndicator()),
-                  );
-          }),
+                            SizedBox(height: screenHeight * 0.015),
+                            Text(
+                              text[index],
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: screenWidth * 0.04,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )
+              : SizedBox(
+                  height: screenHeight * 0.5,
+                  child: const Center(child: CircularProgressIndicator()),
+                ),
         ),
       ),
     );
-    // ));
   }
 }
